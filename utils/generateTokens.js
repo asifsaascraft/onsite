2801da1a271657cef79e40e69a458a7468009255
Asif <asifsaascraft@gmail.com>
2026-03-11 16:26:18 +0530

    update token time

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -4,7 +4,7 @@ export const generateTokens = (userId, role) => {
   const accessToken = jwt.sign(
     { id: userId, role },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES || "15m" }
+    { expiresIn: process.env.JWT_EXPIRES || "7d" }
   );
 
   const refreshToken = jwt.sign(
